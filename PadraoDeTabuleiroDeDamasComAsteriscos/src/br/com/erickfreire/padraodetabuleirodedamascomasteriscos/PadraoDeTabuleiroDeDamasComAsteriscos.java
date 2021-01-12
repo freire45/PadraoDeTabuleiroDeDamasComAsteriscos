7d3fbd4c04e405942438443c664b47d3f433a5ce
Erick Freire <erilefr@gmail.com>
2021-01-12 19:50:09 -0300

    versao1
    versao1

---
+++ PadraoDeTabuleiroDeDamasComAsteriscos/src/br/com/erickfreire/padraodetabuleirodedamascomasteriscos/PadraoDeTabuleiroDeDamasComAsteriscos.java
@@ -0,0 +1,24 @@
+package br.com.erickfreire.padraodetabuleirodedamascomasteriscos;
+
+/**
+ * Crie um aplicativo que simula um tabuleiro de damas com 8 linhas e 8 colunas com asteriscos
+ * @version 1 - Criado em 12-01-2021 as 19:46
+ * @author Erick Freire
+ *
+ */
+
+public class PadraoDeTabuleiroDeDamasComAsteriscos {
+
+	public static void main(String[] args) {
+		System.out.println("* * * * * * * *\n"
+				         + " * * * * * * * *\n"
+				         + "* * * * * * * *\n"
+				         + " * * * * * * * *\n"
+				         + "* * * * * * * * \n"
+				         + " * * * * * * * *\n"
+				         + "* * * * * * * * \n"
+				         + " * * * * * * * *\n");
+
+	}
+
+}
